daa2ca323558a6e9562674094be4fbc4730427c1
Akmalino <fazadandyakmalino0117@gmail.com>
2024-03-21 22:28:13 +0700

    menambahkan file detail html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "detail.html"],
   theme: {
     container: {
       center: true,
