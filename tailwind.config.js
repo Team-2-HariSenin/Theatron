4c68e6c181437af45e8d03f40d5a1e8054dd7d22
fazadandy <fazadandyakmalin0@gmail.com>
2024-06-03 07:57:46 +0700

    settup color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ export default {
         sans: ["Roboto", "sans-serif"],
       },
     },
+    colors: {
+      white: "rgba(246, 246, 246, 1)",
+      "white-70": "rgba(246, 246, 246, 0.7)",
+      black: "rgba(0, 0, 0, 1)",
+      "black-10": "rgba(18, 18, 18, 1)",
+      "black-20": "rgba(26, 26, 26, 1)",
+      "black-30": "rgba(44, 44, 44, 1)",
+      "black-40": "rgba(117, 117, 117, 1)",
+      "black-90": "rgba(33, 33, 33, 0.9)",
+      yellow: "rgba(245, 197, 24, 1)",
+      "light-blue": "rgba(87, 153, 239, 1)",
+    },
   },
   plugins: [],
 };
