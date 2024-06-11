53e6e2f374c367a85cae43e001552932e94abcf1
Erdiansyah205 <erdian054@gmail.com>
2024-06-11 20:32:37 +0700

    text-black pada input search Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
-import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react';
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+} from "@headlessui/react";
 import { GoChevronDown } from "react-icons/go";
 import { BsBookmarkPlus } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -12,34 +18,34 @@ const Navbar = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  const classNames = (...classes) => classes.filter(Boolean).join(' ');
+  const classNames = (...classes) => classes.filter(Boolean).join(" ");
 
   return (
-    <div className="p-2 bg-customColorNavbar text-white">
-      <div className="flex justify-between items-center">
+    <div className="bg-customColorNavbar p-2 text-white">
+      <div className="flex items-center justify-between">
         <button
           onClick={toggleMobileMenu}
-          className="block md:hidden text-white focus:outline-none"
+          className="block text-white focus:outline-none md:hidden"
         >
           <GiHamburgerMenu className="h-6 w-6" />
         </button>
-        <p className="mr-32 hidden lg: block">THEATRON</p>
+        <p className="mr-32 hidden">THEATRON</p>
         <div className="flex items-center gap-2 md:hidden">
           <IoIosSearch />
           <button className="mr-5 w-16">Sign in</button>
         </div>
-        <div className="hidden md:flex items-center justify-center w-full">
+        <div className="hidden w-full items-center justify-center md:flex">
           <p className="mr-5">THEATRON</p>
           <div className="flex items-center gap-10">
             <div className="relative max-w-[400px]">
               <div className="flex items-center">
                 <input
                   type="text"
-                  className="hidden sm:block w-full pl-10 pr-20 py-1 rounded"
+                  className="hidden w-full text-black rounded py-1 pl-10 pr-20 sm:block"
                   placeholder="Search..."
                 />
-                <IoIosSearch className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                <button className="absolute right-0 top-0 h-full bg-white text-black py-1 px-4 rounded-r border-l hidden sm:block">
+                <IoIosSearch className="text-gray-500 absolute left-2 top-1/2 -translate-y-1/2 transform" />
+                <button className="absolute right-0 top-0 hidden h-full rounded-r border-l bg-white px-4 py-1 text-black sm:block">
                   Search
                 </button>
               </div>
@@ -47,7 +53,10 @@ const Navbar = () => {
             <Menu as="div" className="relative inline-block text-left">
               <MenuButton className="inline-flex justify-center gap-x-1.5 bg-customColorNavbar px-3 py-2 text-sm text-white">
                 All Category
-                <GoChevronDown className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                <GoChevronDown
+                  className="text-gray-400 -mr-1 h-5 w-5"
+                  aria-hidden="true"
+                />
               </MenuButton>
               <Transition
                 enter="transition ease-out duration-100"
@@ -59,14 +68,21 @@ const Navbar = () => {
               >
                 <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                   <div className="py-1">
-                    {['Category 1', 'Category 2', 'Category 3', 'Category 4'].map(category => (
+                    {[
+                      "Category 1",
+                      "Category 2",
+                      "Category 3",
+                      "Category 4",
+                    ].map((category) => (
                       <MenuItem key={category}>
                         {({ active }) => (
                           <a
                             href="#"
                             className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm",
                             )}
                           >
                             {category}
@@ -85,7 +101,10 @@ const Navbar = () => {
             <Menu as="div" className="relative inline-block text-left">
               <MenuButton className="inline-flex justify-center gap-x-1.5 bg-customColorNavbar px-3 py-2 text-sm text-white">
                 User
-                <GoChevronDown className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                <GoChevronDown
+                  className="text-gray-400 -mr-1 h-5 w-5"
+                  aria-hidden="true"
+                />
               </MenuButton>
               <Transition
                 enter="transition ease-out duration-100"
@@ -97,29 +116,35 @@ const Navbar = () => {
               >
                 <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                   <div className="py-1">
-                    {['Your Activity', 'Your Watchlist', 'Account Setting'].map(item => (
-                      <MenuItem key={item}>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            {item}
-                          </a>
-                        )}
-                      </MenuItem>
-                    ))}
+                    {["Your Activity", "Your Watchlist", "Account Setting"].map(
+                      (item) => (
+                        <MenuItem key={item}>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-700",
+                                "block px-4 py-2 text-sm",
+                              )}
+                            >
+                              {item}
+                            </a>
+                          )}
+                        </MenuItem>
+                      ),
+                    )}
                     <form method="POST" action="#">
                       <MenuItem>
                         {({ active }) => (
                           <button
                             type="submit"
                             className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block w-full px-4 py-2 text-left text-sm'
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block w-full px-4 py-2 text-left text-sm",
                             )}
                           >
                             Log out
@@ -135,18 +160,18 @@ const Navbar = () => {
         </div>
       </div>
       {isMobileMenuOpen && (
-        <ul className="flex flex-col mt-2 space-y-2 md:hidden">
+        <ul className="mt-2 flex flex-col space-y-2 md:hidden">
           <li className="hidden md:block">THEATRON</li>
           <li className="flex items-center gap-2">
             <div className="relative w-full">
               <div className="flex items-center">
                 <input
                   type="text"
-                  className="w-full pl-10 pr-20 py-1 rounded"
+                  className="w-full rounded py-1 pl-10 pr-20"
                   placeholder="Search..."
                 />
-                <IoIosSearch className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                <button className="absolute right-0 top-0 h-full bg-white text-black py-1 px-4 rounded-r border-l">
+                <IoIosSearch className="text-gray-500 absolute left-2 top-1/2 -translate-y-1/2 transform" />
+                <button className="absolute right-0 top-0 h-full rounded-r border-l bg-white px-4 py-1 text-black">
                   Search
                 </button>
               </div>
@@ -156,7 +181,10 @@ const Navbar = () => {
             <Menu as="div" className="relative inline-block text-left">
               <MenuButton className="inline-flex w-full justify-center gap-x-1.5 bg-customColorNavbar px-3 py-2 text-sm text-white">
                 All Category
-                <GoChevronDown className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                <GoChevronDown
+                  className="text-gray-400 -mr-1 h-5 w-5"
+                  aria-hidden="true"
+                />
               </MenuButton>
               <Transition
                 enter="transition ease-out duration-100"
@@ -168,14 +196,21 @@ const Navbar = () => {
               >
                 <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                   <div className="py-1">
-                    {['Category 1', 'Category 2', 'Category 3', 'Category 4'].map(category => (
+                    {[
+                      "Category 1",
+                      "Category 2",
+                      "Category 3",
+                      "Category 4",
+                    ].map((category) => (
                       <MenuItem key={category}>
                         {({ active }) => (
                           <a
                             href="#"
                             className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block px-4 py-2 text-sm",
                             )}
                           >
                             {category}
@@ -196,7 +231,10 @@ const Navbar = () => {
             <Menu as="div" className="relative inline-block text-left">
               <MenuButton className="inline-flex w-full justify-center gap-x-1.5 bg-customColorNavbar px-3 py-2 text-sm text-white">
                 User
-                <GoChevronDown className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                <GoChevronDown
+                  className="text-gray-400 -mr-1 h-5 w-5"
+                  aria-hidden="true"
+                />
               </MenuButton>
               <Transition
                 enter="transition ease-out duration-100"
@@ -208,29 +246,35 @@ const Navbar = () => {
               >
                 <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                   <div className="py-1">
-                    {['Your Activity', 'Your Watchlist', 'Account Setting'].map(item => (
-                      <MenuItem key={item}>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            {item}
-                          </a>
-                        )}
-                      </MenuItem>
-                    ))}
+                    {["Your Activity", "Your Watchlist", "Account Setting"].map(
+                      (item) => (
+                        <MenuItem key={item}>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-700",
+                                "block px-4 py-2 text-sm",
+                              )}
+                            >
+                              {item}
+                            </a>
+                          )}
+                        </MenuItem>
+                      ),
+                    )}
                     <form method="POST" action="#">
                       <MenuItem>
                         {({ active }) => (
                           <button
                             type="submit"
                             className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block w-full px-4 py-2 text-left text-sm'
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700",
+                              "block w-full px-4 py-2 text-left text-sm",
                             )}
                           >
                             Log out
