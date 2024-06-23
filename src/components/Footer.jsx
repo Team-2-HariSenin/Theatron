1f2bee94ecd0054e02df0292a6d59a634d97ef57
fazadandy <fazadandyakmalin0@gmail.com>
2024-06-23 18:39:24 +0700

    fix navbar and footer width

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
@@ -7,19 +7,33 @@ import { GoChevronRight } from "react-icons/go";
 const Footer = () => {
   return (
     <>
-      <div className='bg-customColorNavbar text-white flex justify-center'>
-        <div className='max-w-screen-lg w-full flex flex-col md:flex-row justify-between py-8 gap-8 md:gap-0'>
-          <div className='w-full md:w-[30%] flex flex-col gap-5 p-4 md:p-8'>
+      <div className="mx-auto flex justify-center bg-black text-white lg:container">
+        <div className="flex w-full max-w-screen-lg flex-col justify-between gap-8 py-8 md:flex-row md:gap-0">
+          <div className="flex w-full flex-col gap-5 p-4 md:w-[30%] md:p-8">
             <h1>Explore</h1>
-            <button className='flex gap-2 items-center'><GoChevronRight /> Thrillers</button>
-            <button className='flex gap-2 items-center'><GoChevronRight /> Action</button>
-            <button className='flex gap-2 items-center'><GoChevronRight /> Anime</button>
-            <button className='flex gap-2 items-center'><GoChevronRight /> All Category</button>
+            <button className="flex items-center gap-2">
+              <GoChevronRight /> Thrillers
+            </button>
+            <button className="flex items-center gap-2">
+              <GoChevronRight /> Action
+            </button>
+            <button className="flex items-center gap-2">
+              <GoChevronRight /> Anime
+            </button>
+            <button className="flex items-center gap-2">
+              <GoChevronRight /> All Category
+            </button>
           </div>
-          <div className='w-full md:w-[50%] flex flex-col gap-5 p-4 md:p-8'>
+          <div className="flex w-full flex-col gap-5 p-4 md:w-[50%] md:p-8">
             <h1>THEATRON</h1>
-            <p>This website draws inspiration from IMDB, offering a curated selection of movie recommendations to help you discover your next favorite movie. Whether you're a casual viewer or a cinephile, our platform is designed to enhance your movie-watching experience with personalized suggestions and detailed reviews.</p>
-            <div className='text-3xl flex gap-4'>
+            <p>
+              This website draws inspiration from IMDB, offering a curated
+              selection of movie recommendations to help you discover your next
+              favorite movie. Whether you're a casual viewer or a cinephile, our
+              platform is designed to enhance your movie-watching experience
+              with personalized suggestions and detailed reviews.
+            </p>
+            <div className="flex gap-4 text-3xl">
               <FaGithub />
               <FaInstagram />
               <FaLinkedin />
@@ -29,6 +43,6 @@ const Footer = () => {
       </div>
     </>
   );
-}
+};
 
 export default Footer;
