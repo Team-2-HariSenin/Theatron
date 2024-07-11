7f09f0048781da0a104a6f835832afe7c78c6a9c
fazadandy <fazadandyakmalin0@gmail.com>
2024-07-12 05:45:41 +0700

    fix : isAdmin in jwt sign

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -9,10 +9,37 @@ const CardMovie = ({ type, id_movie, src, title, rateAverage }) => {
   const [loading, setLoading] = useState(false);
   const { token, isAuthenticated } = useAuthStore((state) => state);
   const [isWatchList, setIsWatchList] = useState(false);
+  const [rating, setRating] = useState(null);
 
-  const { setActive, setTitleMovie, setIdMovie } = useRatingStore(
+  const { setActive, active, setTitleMovie, setIdMovie } = useRatingStore(
     (state) => state,
   );
+
+  const getRating = async () => {
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:3000/api/rate?id_movie=${id_movie}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (response.data.data) {
+        setRating(response.data.data.rate);
+      } else {
+        setRating(null);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      getRating();
+    }
+  }, [active]);
 
   const navigate = useNavigate();
 
@@ -85,13 +112,12 @@ const CardMovie = ({ type, id_movie, src, title, rateAverage }) => {
                 <AddToWatchList loading={loading} />
               )}
             </div>
-            <div className="w-full">
+            <div className="aspect-[23/34] w-full overflow-hidden">
               <img
-                alt="1. Bridgerton"
-                className="object-cover"
+                alt={title}
+                className="h-full w-full object-cover"
                 loading="lazy"
                 src={src}
-                sizes="50vw, (min-width: 480px) 34vw, (min-width: 600px) 26vw, (min-width: 1024px) 16vw, (min-width: 1280px) 16vw"
               />
             </div>
           </div>
@@ -109,25 +135,44 @@ const CardMovie = ({ type, id_movie, src, title, rateAverage }) => {
               >
                 <path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path>
               </svg>
-              {rateAverage.toFixed(1)}
+              {rateAverage === 10 || rateAverage === 0
+                ? rateAverage
+                : rateAverage.toFixed(1)}
             </div>
             <button
               onClick={() => handleAddRating()}
               className="group relative inline-block min-h-8 min-w-12 max-w-full cursor-pointer overflow-hidden rounded bg-tranparent px-3 text-base font-medium tracking-[.03125em] hover:bg-black-30"
             >
-              <span className="text-base font-normal -tracking-tight text-light-blue">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  className="mx-auto h-[0.8em] w-[1em] group-hover:fill-[white]"
-                  viewBox="0 0 24 24"
-                  fill="rgba(87, 153, 239, 1)"
-                  role="presentation"
-                >
-                  <path d="M22.724 8.217l-6.786-.587-2.65-6.22c-.477-1.133-2.103-1.133-2.58 0l-2.65 6.234-6.772.573c-1.234.098-1.739 1.636-.8 2.446l5.146 4.446-1.542 6.598c-.28 1.202 1.023 2.153 2.09 1.51l5.818-3.495 5.819 3.509c1.065.643 2.37-.308 2.089-1.51l-1.542-6.612 5.145-4.446c.94-.81.45-2.348-.785-2.446zm-10.726 8.89l-5.272 3.174 1.402-5.983-4.655-4.026 6.141-.531 2.384-5.634 2.398 5.648 6.14.531-4.654 4.026 1.402 5.983-5.286-3.187z"></path>
-                </svg>
-              </span>
+              {rating ? (
+                <span className="inline-flex items-center justify-center text-base font-normal -tracking-tight text-light-blue">
+                  <svg
+                    width="24"
+                    height="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="mx-auto mr-[.15rem] h-[0.8em] w-[1em] fill-light-blue"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    role="presentation"
+                  >
+                    <path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path>
+                  </svg>
+                  {rating}
+                </span>
+              ) : (
+                <span className="text-base font-normal -tracking-tight text-light-blue">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    className="mx-auto h-[0.8em] w-[1em] group-hover:fill-[white]"
+                    viewBox="0 0 24 24"
+                    fill="rgba(87, 153, 239, 1)"
+                    role="presentation"
+                  >
+                    <path d="M22.724 8.217l-6.786-.587-2.65-6.22c-.477-1.133-2.103-1.133-2.58 0l-2.65 6.234-6.772.573c-1.234.098-1.739 1.636-.8 2.446l5.146 4.446-1.542 6.598c-.28 1.202 1.023 2.153 2.09 1.51l5.818-3.495 5.819 3.509c1.065.643 2.37-.308 2.089-1.51l-1.542-6.612 5.145-4.446c.94-.81.45-2.348-.785-2.446zm-10.726 8.89l-5.272 3.174 1.402-5.983-4.655-4.026 6.141-.531 2.384-5.634 2.398 5.648 6.14.531-4.654 4.026 1.402 5.983-5.286-3.187z"></path>
+                  </svg>
+                </span>
+              )}
             </button>
           </div>
           <Link className="relative h-10 cursor-pointer overflow-hidden text-ellipsis px-2 align-baseline text-base leading-5 tracking-[.03125em]">
@@ -136,7 +181,10 @@ const CardMovie = ({ type, id_movie, src, title, rateAverage }) => {
             </span>
           </Link>
           <div className="relative px-2 pt-3 align-baseline">
-            <Link className="relative inline-flex min-h-9 w-full min-w-12 max-w-full cursor-pointer items-center justify-center overflow-hidden rounded bg-black-30 px-4 text-[0.875rem] font-medium leading-5 tracking-[0.02em] text-light-blue hover:bg-[#433F45]">
+            <Link
+              to={`/detail/${id_movie}`}
+              className="relative inline-flex min-h-9 w-full min-w-12 max-w-full cursor-pointer items-center justify-center overflow-hidden rounded bg-black-30 px-4 text-[0.875rem] font-medium leading-5 tracking-[0.02em] text-light-blue hover:bg-[#433F45]"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -166,13 +214,12 @@ const CardMovie = ({ type, id_movie, src, title, rateAverage }) => {
                 <AddToWatchList loading={loading} />
               )}
             </div>
-            <div className="w-full">
+            <div className="aspect-[23/34] w-full overflow-hidden">
               <img
-                alt="1. Bridgerton"
-                className="object-cover"
+                alt={title}
+                className="h-full w-full object-cover"
                 loading="lazy"
                 src={src}
-                sizes="50vw, (min-width: 480px) 34vw, (min-width: 600px) 26vw, (min-width: 1024px) 16vw, (min-width: 1280px) 16vw"
               />
             </div>
           </div>
@@ -190,25 +237,44 @@ const CardMovie = ({ type, id_movie, src, title, rateAverage }) => {
               >
                 <path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path>
               </svg>
-              {rateAverage.toFixed(1)}
+              {rateAverage === 10 || rateAverage === 0
+                ? rateAverage
+                : rateAverage.toFixed(1)}
             </div>
             <button
               onClick={() => handleAddRating()}
               className="group relative inline-block min-h-8 min-w-12 max-w-full cursor-pointer overflow-hidden rounded bg-tranparent px-3 text-base font-medium tracking-[.03125em] hover:bg-black-30"
             >
-              <span className="text-base font-normal -tracking-tight text-light-blue">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  className="mx-auto h-[0.8em] w-[1em] group-hover:fill-[white]"
-                  viewBox="0 0 24 24"
-                  fill="rgba(87, 153, 239, 1)"
-                  role="presentation"
-                >
-                  <path d="M22.724 8.217l-6.786-.587-2.65-6.22c-.477-1.133-2.103-1.133-2.58 0l-2.65 6.234-6.772.573c-1.234.098-1.739 1.636-.8 2.446l5.146 4.446-1.542 6.598c-.28 1.202 1.023 2.153 2.09 1.51l5.818-3.495 5.819 3.509c1.065.643 2.37-.308 2.089-1.51l-1.542-6.612 5.145-4.446c.94-.81.45-2.348-.785-2.446zm-10.726 8.89l-5.272 3.174 1.402-5.983-4.655-4.026 6.141-.531 2.384-5.634 2.398 5.648 6.14.531-4.654 4.026 1.402 5.983-5.286-3.187z"></path>
-                </svg>
-              </span>
+              {rating ? (
+                <span className="inline-flex items-center justify-center text-base font-normal -tracking-tight text-light-blue">
+                  <svg
+                    width="24"
+                    height="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="mx-auto mr-[.15rem] h-[0.8em] w-[1em] fill-light-blue"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    role="presentation"
+                  >
+                    <path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path>
+                  </svg>
+                  {rating}
+                </span>
+              ) : (
+                <span className="text-base font-normal -tracking-tight text-light-blue">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    className="mx-auto h-[0.8em] w-[1em] group-hover:fill-[white]"
+                    viewBox="0 0 24 24"
+                    fill="rgba(87, 153, 239, 1)"
+                    role="presentation"
+                  >
+                    <path d="M22.724 8.217l-6.786-.587-2.65-6.22c-.477-1.133-2.103-1.133-2.58 0l-2.65 6.234-6.772.573c-1.234.098-1.739 1.636-.8 2.446l5.146 4.446-1.542 6.598c-.28 1.202 1.023 2.153 2.09 1.51l5.818-3.495 5.819 3.509c1.065.643 2.37-.308 2.089-1.51l-1.542-6.612 5.145-4.446c.94-.81.45-2.348-.785-2.446zm-10.726 8.89l-5.272 3.174 1.402-5.983-4.655-4.026 6.141-.531 2.384-5.634 2.398 5.648 6.14.531-4.654 4.026 1.402 5.983-5.286-3.187z"></path>
+                  </svg>
+                </span>
+              )}
             </button>
           </div>
           <Link className="relative h-10 cursor-pointer overflow-hidden text-ellipsis px-2 align-baseline text-base leading-5 tracking-[.03125em]">
@@ -256,7 +322,10 @@ const CardMovie = ({ type, id_movie, src, title, rateAverage }) => {
               )}
             </button>
             <div className="-mb-4 flex w-full items-center justify-center py-2 tracking-[.03125em]">
-              <Link className="relative inline-flex min-h-9 min-w-12 max-w-full cursor-pointer items-center justify-center overflow-hidden text-ellipsis rounded px-4 align-baseline text-[0.875rem] font-medium leading-5 tracking-[0.02em] text-white hover:bg-[#403737]">
+              <Link
+                to={`/detail/${id_movie}`}
+                className="relative inline-flex min-h-9 min-w-12 max-w-full cursor-pointer items-center justify-center overflow-hidden text-ellipsis rounded px-4 align-baseline text-[0.875rem] font-medium leading-5 tracking-[0.02em] text-white hover:bg-[#403737]"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
